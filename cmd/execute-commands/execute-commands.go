256b5b735b352327d36f93c4056db56b3262a348
Jonathan Pérez <nighter.7@gmail.com>
2018-07-18 19:19:52 -0500

    sending commands as files

--- cmd/execute-commands/execute-commands.go
+++ cmd/execute-commands/execute-commands.go
@@ -4,19 +4,27 @@ import (
 	"fmt"
 	"os/exec"
 	"runtime"
+	"io/ioutil"
 )
 
 func main() {
 	// Getting os name
 	fmt.Println(runtime.GOOS)
 
-	// Executing commands
-	cmd := exec.Command(`tasklist`)
+	// Executing commands	
+	txt := []byte(`tasklist | find "chrome.exe"`)
+	err := ioutil.WriteFile("tmpfile.bat", txt, 0644)
+	
+	if err != nil {
+		fmt.Printf("stderr: %s\n", err)
+	}
+	
+	cmd := exec.Command("tmpfile.bat")
 
-	stdout, erro := cmd.CombinedOutput()
+	stdout, err := cmd.CombinedOutput()
 
-	if erro != nil {
-		fmt.Printf("stderr: %s\n", erro)
+	if err != nil {
+		fmt.Printf("stderr: %s\n", err)
 	}
 
 	fmt.Printf("stdout: %s\n", stdout)
